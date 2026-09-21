f09ea92e20adb51b1fa30c851084b5a15d26490e
michaelstborbor <michaelstborbor@gmail.com>
2026-09-21 21:42:20 +0000

    Delete idts_backend directory

--- idts_backend/app/schemas/user.py
+++
@@ -1,9 +0,0 @@
-import uuid
-from typing import Optional
-
-from pydantic import BaseModel, Field
-
-
-class CHWCreate(BaseModel):
-    full_name: str = Field(min_length=1, max_length=200)
-    facility_id: Optional[uuid.UUID] = None
